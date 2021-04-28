88c2442c1bc1ff976ea08be059a54ac8af9304aa
Ben Carlson <bjcarlson42@outlook.com>
2021-04-28 16:35:45 -0400

    update: Add Google analytics.

--- pages/_document.js
+++ pages/_document.js
@@ -2,13 +2,32 @@ import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript } from "@chakra-ui/react"
 import theme from '../styles/theme'
 import { GoogleFonts } from 'next-google-fonts'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends NextDocument {
     render() {
         return (
             <Html lang="en">
                 <GoogleFonts href="https://fonts.googleapis.com/css?family=Inter&display=swap" />
-                <Head></Head>
+                <Head>
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                        }}
+                    />
+                </Head>
                 <body>
                     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
                     <Main />

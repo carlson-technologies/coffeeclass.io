5121e09997492e08e5f6e079b4877f6620059749
Ben Carlson <bjcarlson42@outlook.com>
2021-07-01 15:00:33 -0400

    update: Add giscus comments.

--- components/Comments.js
+++ components/Comments.js
@@ -1,3 +1,5 @@
+// coffeeclass.io uses giscus for comments. https://giscus.app/
+
 import React, { Component } from "react";
 export default class Comments extends Component {
     constructor(props) {
@@ -6,15 +8,17 @@ export default class Comments extends Component {
     }
 
     componentDidMount() {
-        let colorMode = localStorage.getItem('chakra-ui-color-mode');
-        const utteranceTheme = colorMode === "dark" ? "github-dark" : "github-light";
         let scriptEl = document.createElement("script");
-        scriptEl.setAttribute("src", "https://utteranc.es/client.js");
+        scriptEl.setAttribute("src", "https://giscus.app/client.js");
+        scriptEl.setAttribute("data-repo", "carlson-technologies/coffeeclass.io");
+        scriptEl.setAttribute("data-repo-id", "MDEwOlJlcG9zaXRvcnkzNTQxODIwODI=");
+        scriptEl.setAttribute("data-category", "Website Comments");
+        scriptEl.setAttribute("data-category-id", "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMzMDc3MDQ5");
+        scriptEl.setAttribute("data-mapping", "url");
+        scriptEl.setAttribute("data-reactions-enabled", "1");
+        scriptEl.setAttribute("data-theme", "preferred_color_scheme");
         scriptEl.setAttribute("crossorigin", "anonymous");
         scriptEl.setAttribute("async", true);
-        scriptEl.setAttribute("repo", "coffeeclass/comments-coffeeclass.io");
-        scriptEl.setAttribute("issue-term", "og:title");
-        scriptEl.setAttribute("theme", utteranceTheme);
         this.commentBox.current.appendChild(scriptEl);
     }
 

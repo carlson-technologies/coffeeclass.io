4842785893032bc63c5146ac6d041ca12a0a55ff
Ben Carlson <bjcarlson42@outlook.com>
2021-12-08 12:28:11 -0500

    update: About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import {
     Flex,
     Stack,
     Text,
-    Divider,
+    Box,
     useColorMode,
     Link
 } from '@chakra-ui/react'
@@ -19,10 +19,7 @@ export default function About() {
         light: 'gray.700',
         dark: 'gray.300'
     }
-    const headerColor = {
-        light: 'brand_one.600',
-        dark: 'brand_one.500'
-    }
+
     return (
         <Container title={title} description={description} url={url}>
             <Stack
@@ -34,16 +31,8 @@ export default function About() {
                     w={["100%", "100%", "100%", "100%", "100%", "60%"]}
                     alignSelf="center"
                 >
-                    <Heading
-                        as="h1"
-                        size="2xl"
-                        letterSpacing="tight"
-                        textTransform="uppercase"
-                        my={8}
-                    >
-                        About coffeeclass.io 👋
-                    </Heading>
-                    <Divider mt={2} />
+                    <Heading mt={4} as="h1" size="2xl" color="brand_one.500">About coffeeclass.io 👋</Heading>
+                    <Box bgColor="brand_one.500" h={2} w={150} borderRadius={5} mb={4} mt={2} />
                     <Heading as="h2" size="lg" mb={2} mt={4} fontWeight={700} letterSpacing="tight">Who Is coffeeclass.io?</Heading>
                     <Text mb={2} color={color[colorMode]} fontSize="large">coffeeclass.io is created by <Link color="brand_one.500" href="https://benjamincarlson.io" isExternal>Benjamin Carlson</Link>.</Text>
 
@@ -54,14 +43,14 @@ export default function About() {
                     <Text mb={2} color={color[colorMode]} fontSize="large">coffeeclass.io is 100% online.</Text>
 
                     <Heading as="h2" size="lg" my={2} mt={4} fontWeight={700} letterSpacing="tight">When Was coffeeclass.io Created?</Heading>
-                    <Text mb={2} color={color[colorMode]} fontSize="large">Ben started creating programming tutorials in April 2020 on his website and YouTube channel, but re-branded to coffeeclass.io in April 2021.</Text>
+                    <Text mb={2} color={color[colorMode]} fontSize="large">Ben started creating programming tutorials in April 2020 on his <Link color="brand_one.500" href="https://benjamincarlson.io" isExternal>website</Link> and YouTube channel. In April 2021, Ben stopped writing tutorials on his personal website and created this website.</Text>
 
                     <Heading as="h2" size="lg" my={2} mt={4} fontWeight={700} letterSpacing="tight">Why Was coffeeclass.io Created?</Heading>
                     <Text mb={2} color={color[colorMode]} fontSize="large">To present complex programming ideas in an easy, and fun, to follow format.</Text>
 
-                    <Heading as="h2" size="lg" my={2} mt={4} fontWeight={700} letterSpacing="tight">How Do I Use coffeeclass.io?</Heading>
+                    {/* <Heading as="h2" size="lg" my={2} mt={4} fontWeight={700} letterSpacing="tight">How Do I Use coffeeclass.io?</Heading>
                     <Text mb={2} color={color[colorMode]} fontSize="large">This is completely up to you! This site is growing by the day but at the moment, our three main services
-                        are <Link color="brand_one.500" href="/tutorials">coffeeclass.io Tutorials</Link>, <Link color="brand_one.500" href="/snippets">coffeeclass.io Snippets</Link>, and <Link color="brand_one.500" href="/learn">coffeeclass.io Learn</Link>.</Text>
+                        are <Link color="brand_one.500" href="/tutorials">coffeeclass.io Tutorials</Link>, <Link color="brand_one.500" href="/snippets">coffeeclass.io Snippets</Link>, and <Link color="brand_one.500" href="/learn">coffeeclass.io Learn</Link>.</Text> */}
                 </Flex>
             </Stack>
         </Container>

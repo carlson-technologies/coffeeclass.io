2637dcf06ecc39f3f0e1b7b56bc0f274cb1f9b34
Ben Carlson <bjcarlson42@outlook.com>
2022-01-02 20:26:08 -0500

    make step linkable

--- src/components/Content/Step.tsx
+++ src/components/Content/Step.tsx
@@ -9,20 +9,6 @@ type StepProps = {
 };
 
 const Step = ({ number, title }: StepProps) => {
-
-    // for (let i = 0; i < title.length; i++) {
-    //     if (title[i] == '`') {
-    //         title = title.replace('`', '<code>')
-
-    //         for (let j = i + 1; j < title.length; j++) {
-    //             if (title[j] == '`') {
-    //                 title = title.replace('`', '</code>')
-    //                 break
-    //             }
-    //         }
-    //     }
-    // }
-
     for (let i = 0; i < title.length; i++) {
         if (title[i] == '`') {
             title = title.replace('`', '')
@@ -34,7 +20,7 @@ const Step = ({ number, title }: StepProps) => {
             <Flex borderRadius="50%" border="1px solid" w={35} h={35} alignItems="center" justifyContent="center" mr={2}>
                 {number}
             </Flex>
-            <Heading as="h2" size="md">{title}</Heading>
+            <Heading as="h2" size="md" id={title}>{title}</Heading>
         </Flex>
     )
 }

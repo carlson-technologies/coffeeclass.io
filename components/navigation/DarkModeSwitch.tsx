2b080f81cd0fb2b52f682cccf6b65da1d1a8d338
Benjamin Carlson <bjcarlson42@gmail.com>
2023-11-06 17:26:35 -0500

    fix: color mode (#88)
    * fix: dark mode switch
    
    * fix: color mode

--- components/navigation/DarkModeSwitch.tsx
+++ components/navigation/DarkModeSwitch.tsx
@@ -1,47 +1,31 @@
 import { useColorMode, IconButton } from '@chakra-ui/react'
 import { useTheme } from 'next-themes'
+import React from 'react'
 import { FiSun, FiMoon } from 'react-icons/fi'
 
 const DarkModeSwitch = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const { colorMode, setColorMode } = useColorMode()
   const { theme, setTheme } = useTheme()
+
+  const handleThemeChange = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+    setColorMode(theme === 'light' ? 'dark' : 'light')
+  }
 
   return (
     <>
       {colorMode === 'dark' ? (
         <FiSun
           className="hover:opacity-80 text-lg cursor-pointer"
-          onClick={() => {
-            toggleColorMode()
-            setTheme(theme === 'light' ? 'dark' : 'light')
-          }}
+          onClick={handleThemeChange}
         />
       ) : (
         <FiMoon
           className="hover:opacity-80 text-lg cursor-pointer"
-          onClick={() => {
-            toggleColorMode()
-            setTheme(theme === 'light' ? 'dark' : 'light')
-          }}
+          onClick={handleThemeChange}
         />
       )}
     </>
-    // <IconButton
-    //   size="md"
-    //   aria-label={`Toggle ${colorMode === 'dark' ? 'light' : 'dark'} mode`}
-    //   icon={colorMode === 'dark' ? <FiSun /> : <FiMoon />}
-    //   fontSize="20px"
-    //   onClick={() => {
-    //     toggleColorMode()
-    //     setTheme(theme === 'light' ? 'dark' : 'light')
-    //   }}
-    //   borderRadius={5}
-    //   variant="ghost"
-    //   transition="transform 1s"
-    //   w={50}
-    //   data-splitbee-event="Button Click"
-    //   data-splitbee-event-type="Dark Mode Toggle"
-    // />
   )
 }
 
